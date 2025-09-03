edbb198baa67df09cc2d4a814e4e9830c585ad08
Ktsarvi <alasgarovtimur@gmail.com>
2025-09-04 01:39:27 +0400

    implement homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from "@/components/companioncard";
+import CompanionList from "@/components/companionlist";
+import Cta from "@/components/cta";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>Let's get started</Button> 
-    </div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Topic: English Literature"
+          subject="language"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
 
-export default Page
+      <section className="home-section">
+        <CompanionList 
+        title="Recently completed lessons"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
