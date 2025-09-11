41dbcd598d1095800df559a781585ee73c57d69d
Ktsarvi <alasgarovtimur@gmail.com>
2025-09-11 23:49:39 +0400

    implement Vapi setup and header

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -18,12 +18,18 @@ export const createCompanion = async (formData: CreateCompanion) => {
   return data[0];
 };
 
-export const getAllCompanions = async ( {limit=10, page=1, subject, topic}: GetAllCompanions ) => {
+export const getAllCompanions = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllCompanions) => {
   const supabase = createSupabaseClient();
 
   let query = supabase.from("companions").select();
   if (subject && topic) {
-    query = query.ilike("subject", `%${subject}%`)
+    query = query
+      .ilike("subject", `%${subject}%`)
       .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`);
   } else if (subject) {
     query = query.ilike("subject", `%${subject}%`);
@@ -36,7 +42,19 @@ export const getAllCompanions = async ( {limit=10, page=1, subject, topic}: GetA
 
   if (error) {
     throw new Error(error.message);
-  } 
+  }
 
   return companions;
-}
+};
+
+export const getCompanion = async (id: string) => {
+  const supabase = createSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select()
+    .eq("id", id);
+
+  if (error) return console.log(error);
+  return data[0];
+};
