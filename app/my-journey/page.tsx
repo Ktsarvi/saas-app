2c00bfa119fbfc81a8338f130837d2dc3af116f1
Ktsarvi <alasgarovtimur@gmail.com>
2025-09-15 21:19:41 +0400

    implement bookmarks

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -1,17 +1,18 @@
-import CompanionList from "@/components/companionlist";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import {
   getUserCompanions,
   getUserSessions,
+  getBookmarkedCompanions,
 } from "@/lib/actions/companion.actions";
-import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
-import { redirect } from "next/navigation";
+import CompanionList from "@/components/companionlist";
 
 const Profile = async () => {
   const user = await currentUser();
@@ -22,6 +23,7 @@ const Profile = async () => {
 
   const companions = await getUserCompanions(user.id);
   const sessionHistory = await getUserSessions(user.id);
+  const bookmarkedCompanions = await getBookmarkedCompanions(user.id);
 
   return (
     <main className="min-lg:w-3/4">
@@ -47,7 +49,12 @@ const Profile = async () => {
         <div className="flex gap-4">
           <div className="border border-black rounded-lg p-3 gap-2 flex flex-col h-fit">
             <div className="flex gap-2 items-center">
-              <Image src="/icons/check.svg" alt="checkmark " width={22} height={22} />
+              <Image
+                src="/icons/check.svg"
+                alt="checkmark "
+                width={22}
+                height={22}
+              />
               <p className="text-2xl font-bold">{sessionHistory.length}</p>
             </div>
             <div>Lessons Completed</div>
@@ -55,7 +62,12 @@ const Profile = async () => {
 
           <div className="border border-black rounded-lg p-3 gap-2 flex flex-col h-fit">
             <div className="flex gap-2 items-center">
-              <Image src="/icons/cap.svg" alt="capmark " width={22} height={22} />
+              <Image
+                src="/icons/cap.svg"
+                alt="capmark "
+                width={22}
+                height={22}
+              />
               <p className="text-2xl font-bold">{companions.length}</p>
             </div>
             <div>Companions Created</div>
@@ -64,14 +76,32 @@ const Profile = async () => {
       </section>
 
       <Accordion type="multiple">
-        <AccordionItem value="recent">
-          <AccordionTrigger className="text-2xl font-bold">Recent Sessions</AccordionTrigger>
+        <AccordionItem value="bookmarks">
+          <AccordionTrigger className="text-2xl font-bold">
+            Bookmarked Companions {`(${bookmarkedCompanions.length})`}
+          </AccordionTrigger>
           <AccordionContent>
-            <CompanionList title="Recent Sessions" companions={sessionHistory} />
+            <CompanionList
+              companions={bookmarkedCompanions}
+              title="Bookmarked Companions"
+            />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="recent">
+          <AccordionTrigger className="text-2xl font-bold">
+            Recent Sessions
+          </AccordionTrigger>
+          <AccordionContent>
+            <CompanionList
+              title="Recent Sessions"
+              companions={sessionHistory}
+            />
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="companions">
-          <AccordionTrigger className="text-2xl font-bold">My Companions {`(${companions.length})`}</AccordionTrigger>
+          <AccordionTrigger className="text-2xl font-bold">
+            My Companions {`(${companions.length})`}
+          </AccordionTrigger>
           <AccordionContent>
             <CompanionList title="My Companions" companions={companions} />
           </AccordionContent>
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { createSupabaseClient } from "../supabase";
 import { th } from "zod/locales";
+import { revalidatePath } from "next/cache";
 
 export const createCompanion = async (formData: CreateCompanion) => {
   const { userId: author } = await auth();
@@ -28,22 +29,22 @@ export const getAllCompanions = async ({
   const supabase = createSupabaseClient();
 
   let query = supabase.from("companions").select();
+
   if (subject && topic) {
     query = query
       .ilike("subject", `%${subject}%`)
-      .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`);
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
   } else if (subject) {
     query = query.ilike("subject", `%${subject}%`);
   } else if (topic) {
-    query = query.ilike("topic", `%${topic}%, name.ilike.%${topic}%`);
+    query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
   }
 
   query = query.range((page - 1) * limit, page * limit - 1);
+
   const { data: companions, error } = await query;
 
-  if (error) {
-    throw new Error(error.message);
-  }
+  if (error) throw new Error(error.message);
 
   return companions;
 };
@@ -111,7 +112,7 @@ export const getUserCompanions = async (userId: string) => {
     .from("companions")
     .select()
     .eq("author", userId)
-    .order("created_at", { ascending: false })
+    .order("created_at", { ascending: false });
 
   if (error) {
     throw new Error(error.message);
@@ -132,7 +133,7 @@ export const newCompanionPermissions = async () => {
     limit = 3;
   } else if (has({ feature: "10_active_companions" })) {
     limit = 10;
-  } 
+  }
 
   const { data, error } = await supabase
     .from("companions")
@@ -141,7 +142,7 @@ export const newCompanionPermissions = async () => {
 
   if (error) {
     throw new Error(error.message);
-  } 
+  }
 
   const companionCount = data?.length;
 
@@ -150,4 +151,51 @@ export const newCompanionPermissions = async () => {
   } else {
     return true;
   }
-}
+};
+
+export const addBookmark = async (companionId: string, path: string) => {
+  const { userId } = await auth();
+  if (!userId) return;
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase.from("bookmarks").insert({
+    companion_id: companionId,
+    user_id: userId,
+  });
+  if (error) {
+    throw new Error(error.message);
+  }
+  // Revalidate the path to force a re-render of the page
+
+  revalidatePath(path);
+  return data;
+};
+
+export const removeBookmark = async (companionId: string, path: string) => {
+  const { userId } = await auth();
+  if (!userId) return;
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase
+    .from("bookmarks")
+    .delete()
+    .eq("companion_id", companionId)
+    .eq("user_id", userId);
+  if (error) {
+    throw new Error(error.message);
+  }
+  revalidatePath(path);
+  return data;
+};
+
+// It's almost the same as getUserCompanions, but it's for the bookmarked companions
+export const getBookmarkedCompanions = async (userId: string) => {
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase
+    .from("bookmarks")
+    .select(`companions:companion_id (*)`) // Notice the (*) to get all the companion data
+    .eq("user_id", userId);
+  if (error) {
+    throw new Error(error.message);
+  }
+  // We don't need the bookmarks data, so we return only the companions
+  return data.map(({ companions }) => companions);
+};
